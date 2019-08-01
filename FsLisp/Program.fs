d2d96b7b74ecd21fcce42cbe0c9b37bb7386af3d
Matthew Herman <mattherman11@gmail.com>
2019-08-01 10:35:01 -0500

    Implemented repl logic

--- FsLisp/Program.fs
+++ FsLisp/Program.fs
@@ -1,9 +1,21 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let eval (text: string) =
+    text
+
+let print (result: string)=
+    Console.WriteLine(result)
+
+let rec repl () =
+    Console.Write("* ");
+    () |> Console.ReadLine
+       |> eval
+       |> print
+       |> repl
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    System.Threading.Thread.Sleep(5000)
+    printfn "Running FsLisp interpreter..."
+    repl()
     0 // return an integer exit code
+
